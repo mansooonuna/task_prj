62217a5b2995f6ae5f719e9257a768bbc5dc33f9
mansooonuna <parkdasom95@gmail.com>
2023-08-17 15:50:26 +0900

    Feat : util 추가(Message, Timestamped)

---
+++ src/main/java/com/mailplug/homework/util/Timestamped.java
@@ -0,0 +1,21 @@
+package com.mailplug.homework.util;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamped {
+    @CreatedDate
+    private LocalDateTime create_at;
+
+    @LastModifiedDate
+    private LocalDateTime update_at;
+}
