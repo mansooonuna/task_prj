e18759d5a54d53c4f888853696e5af204ae347b7
mansooonuna <parkdasom95@gmail.com>
2023-08-17 17:17:20 +0900

    Refactor : 요구사항에 맞게 날짜형식 수정

--- src/main/java/com/mailplug/homework/util/Timestamped.java
+++ src/main/java/com/mailplug/homework/util/Timestamped.java
@@ -1,9 +1,11 @@
 package com.mailplug.homework.util;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
@@ -14,8 +16,10 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class Timestamped {
     @CreatedDate
+    @JsonFormat(pattern = "yy-MM-dd")
     private LocalDateTime create_at;
 
     @LastModifiedDate
+    @JsonFormat(pattern = "yy-MM-dd")
     private LocalDateTime update_at;
 }
