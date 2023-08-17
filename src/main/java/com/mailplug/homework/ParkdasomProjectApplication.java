dc73c53015a5c21a4635a735b65ec191ebd5a76c
mansooonuna <parkdasom95@gmail.com>
2023-08-17 09:23:38 +0900

    Chore : 프로젝트 초기 설정 완료

---
+++ src/main/java/com/mailplug/homework/ParkdasomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mailplug.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ParkdasomProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ParkdasomProjectApplication.class, args);
+    }
+}
