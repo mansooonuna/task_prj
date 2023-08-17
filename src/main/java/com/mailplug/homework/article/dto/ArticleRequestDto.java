3ff5c63cc74b0fa14eb4b84121e392e00136fcf3
mansooonuna <parkdasom95@gmail.com>
2023-08-17 18:31:34 +0900

    Feat : ArticleRequestDto에 @Builder 어노테이션 추가

--- src/main/java/com/mailplug/homework/article/dto/ArticleRequestDto.java
+++ src/main/java/com/mailplug/homework/article/dto/ArticleRequestDto.java
@@ -1,9 +1,15 @@
 package com.mailplug.homework.article.dto;
 
 import com.mailplug.homework.article.entity.Board;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ArticleRequestDto {
     private String title;
     private String contents;
