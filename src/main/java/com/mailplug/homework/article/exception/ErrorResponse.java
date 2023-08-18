9006bdbf0e96eed957b4180b42f907405c4869db
mansooonuna <parkdasom95@gmail.com>
2023-08-18 22:39:34 +0900

    Refactor : Error 반환 변수명 수정

--- src/main/java/com/mailplug/homework/article/exception/ErrorResponse.java
+++ src/main/java/com/mailplug/homework/article/exception/ErrorResponse.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 public class ErrorResponse {
     private String message;
     private int status;
-    private String detail;
+    private String data;
 
     // 에러 반환 형식
     public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
@@ -34,12 +34,12 @@ public class ErrorResponse {
                 );
     }
 
-    public static ResponseEntity<ErrorResponse> toResponseEntity(HttpStatus httpStatus, String detail) {
+    public static ResponseEntity<ErrorResponse> toResponseEntity(HttpStatus httpStatus, String data) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.builder()
                         .message(httpStatus.name())
-                        .detail(detail)
+                        .data(data)
                         .status(httpStatus.value())
                         .build()
                 );
