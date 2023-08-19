18248ffde9f48a6ab462878af82dde069dbc1ec5
mansooonuna <parkdasom95@gmail.com>
2023-08-19 18:37:24 +0900

    Docs : Api 명세 파악위해 스웨거 적용

--- src/main/java/com/mailplug/homework/article/controller/ArticleController.java
+++ src/main/java/com/mailplug/homework/article/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.mailplug.homework.article.dto.ArticleRequestDto;
 import com.mailplug.homework.article.entity.Board;
 import com.mailplug.homework.article.service.ArticleService;
 import com.mailplug.homework.util.Message;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -19,7 +20,7 @@ import javax.validation.Valid;
 public class ArticleController {
     private final ArticleService articleService;
 
-    // 선택한 게시판에 속한 게시글 목록 조회 & 게시글 제목 검색
+    @Operation(summary = "선택한 게시판에 속한 게시글 목록 조회 & 게시글 제목 검색 API")
     @GetMapping
     public ResponseEntity<Message> getArticles(@RequestParam(required = false) Board name,
                                                @PageableDefault(size = 10, sort = "createAt", direction = Sort.Direction.DESC) Pageable pageable,
@@ -27,20 +28,21 @@ public class ArticleController {
         return articleService.getArticles(name, pageable, searchKeyword);
     }
 
-    // 게시글 단건 조회
+    @Operation(summary = "게시글 단건 조회 API")
     @GetMapping("/{articleId}")
     public ResponseEntity<Message> getArticle(@PathVariable Long articleId) {
         return articleService.getArticle(articleId);
     }
 
-    // 게시글 등록
+    @Operation(summary = "게시글 등록 API")
     @PostMapping("/add")
     public ResponseEntity<Message> addArticle(@RequestHeader(name = "X-USERID") String userId,
                                               @Valid @RequestBody ArticleRequestDto requestDto) {
         return articleService.addArticle(userId, requestDto);
     }
 
-    // 게시글 수정
+
+    @Operation(summary = "게시글 수정 API")
     @PutMapping("/{articleId}")
     public ResponseEntity<Message> updateArticle(@PathVariable Long articleId,
                                                  @RequestHeader(name = "X-USERID") String userId,
@@ -48,7 +50,7 @@ public class ArticleController {
         return articleService.updateArticle(articleId, userId, requestDto);
     }
 
-    // 게시글 삭제
+    @Operation(summary = "게시글 삭제 API")
     @DeleteMapping("/{articleId}")
     public ResponseEntity<Message> deleteArticle(@PathVariable Long articleId,
                                                  @RequestHeader(name = "X-USERID") String userId) {
